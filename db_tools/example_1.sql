36318711c2040a72356b881b12940d5573cf8af7
Chenglong <mestway@gmail.com>
2018-05-25 10:11:57 -0700

    ok

--- db_tools/example_1.sql
+++
@@ -1,4 +0,0 @@
-select d.deptno, min(e.empid) from test_db.emps as e join test_db.depts as d on e.deptno = d.deptno group by d.deptno having count(*) > 1;
-select d.deptno, max(e.empid) from test_db.emps as e join test_db.depts as d on e.deptno = d.deptno group by d.deptno having count(*) > 1;
-select d.deptno, min(e.empid) from test_db.emps as e join test_db.depts as d on e.deptno = d.deptno group by d.deptno having count(*) > 0;
-select d.deptno, min(e.empid) from test_db.emps as e join test_db.depts as d on e.deptno = d.deptno group by d.deptno;
