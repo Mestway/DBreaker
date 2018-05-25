0282b0149772a2b0bed47c0d91d4544d00f5b9ed
Chenglong <mestway@gmail.com>
2018-05-25 15:50:26 -0700

    loca

--- build_project.sh
+++ build_project.sh
@@ -1,3 +1,13 @@
+# replace "-" with "_" for database username
+MAINDB="test_database"
+USER_NAME="dbtest"
+PASSWORD="dbtest"
+
+#mysql -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+#mysql -e "CREATE USER ${USER_NAME}@localhost IDENTIFIED BY '${PASSWORD}';"
+#mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${USER_NAME}'@'localhost';"
+#mysql -e "FLUSH PRIVILEGES;"
+
 cd calcite_interface
 mvn clean compile assembly:single
 cd ..
